67a315c81623ed09801c614033ffefe3aa27ee49
Angie Restanio <arestani@uccs.edu>
2023-09-15 22:20:50 -0600

    add static files

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-
+import os
 
 from pathlib import Path
 
@@ -125,7 +125,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
