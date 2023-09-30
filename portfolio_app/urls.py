a571ce2ab468b40a4c49d1d4cb067d5586563120
Angie Restanio <arestani@uccs.edu>
2023-09-30 00:29:28 -0600

    create student model and add to database

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # defines url patter
+    # defines url pattern
     # '' matches root url of app, then maps to index function from views
-    # 'index' is name accosiated with this url
+    # 'index' is name associated with this url
     path('', views.index, name='index'),
 ]
