f878602aa0f23eb6f25042a8521cc72ab2939e8b
Angie Restanio <arestani@uccs.edu>
2023-10-14 21:29:15 -0600

    update views and template and create menu

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio_app',
+    'bootstrap5',
 ]
 
 # Add support for authenticating users
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
+from django.views import generic
 
 # Create your views here.
 def index(request):
     # Render the HTML template index.html with the data in the context variable.
     return render(request, 'portfolio_app/index.html')
+
+class StudentListView(generic.ListView):
+    model = Student
+
+class StudentDetailView(generic.DetailView):
+    model = Student
