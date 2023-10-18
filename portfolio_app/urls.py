9a2348b4297e32727b329ff35b58be004d2cf301
Angie Restanio <arestani@uccs.edu>
2023-10-18 14:11:46 -0600

    add project form to create a project

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('portfolio/<int:pk>', views.PortfolioDetailView.as_view(), name='portfolio-detail'),
     path('projects/', views.ProjectListView.as_view(), name= 'projects'),
     path('project/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
+    path('portfolio/<int:portfolio_id>/create_project/', views.createProject, name='create_project'),
 ] 
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.views import generic
-# from .forms import ProjectForm, PortfolioForm
+from .forms import ProjectForm #, PortfolioForm
 from django.contrib import messages
 
 # Create your views here.
@@ -45,3 +45,28 @@ class ProjectListView(generic.ListView):
 
 class ProjectDetailView(generic.DetailView):
     model = Project
+
+def createProject(request, portfolio_id):
+    form = ProjectForm()
+    portfolio = Portfolio.objects.get(pk=portfolio_id)
+    
+    if request.method == 'POST':
+        # Create a new dictionary with form data and portfolio_id
+        project_data = request.POST.copy() # copy of post data
+        project_data['portfolio_id'] = portfolio_id # associate new project with correct portfolio
+        
+        # create new instance of ProjectForm with updated data
+        form = ProjectForm(project_data)
+        if form.is_valid():
+            # Save the form without committing to the database
+            project = form.save(commit=False)
+            # Set the portfolio relationship
+            project.portfolio = portfolio
+            # save to database
+            project.save()
+
+            # Redirect back to the portfolio detail page
+            return redirect('portfolio-detail', portfolio_id)
+
+    context = {'form': form}
+    return render(request, 'portfolio_app/project_form.html', context)
